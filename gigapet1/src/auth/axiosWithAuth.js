31f581928156aaf77168430ead4516398bbdf152
newbi462 <isthisemailfree@gmail.com>
2020-01-06 20:57:59 -0500

    BE to date

--- gigapet1/src/auth/axiosWithAuth.js
+++ gigapet1/src/auth/axiosWithAuth.js
@@ -3,9 +3,20 @@ import axios from "axios";
 export const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
   return axios.create({
-    baseURL: "https://gigapetbw4.herokuapp.com/api/auth",
+    baseURL: "https://gigapetbw4.herokuapp.com/",
     headers: {
       authorization: token
     }
   });
 };
+
+
+/*
+register a user == https://gigapetbw4.herokuapp.com/auth/register
+
+User login == https://gigapetbw4.herokuapp.com/auth/login
+*Needs token
+
+User info == https://gigapetbw4.herokuapp.com/users/pet
+
+*/
