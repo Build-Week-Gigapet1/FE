74b890a1f79f15d2c39c4a15bac115b81feeacf7
newbi462 <isthisemailfree@gmail.com>
2020-01-08 18:15:36 -0500

    Menu Tweak

--- gigapet1/src/components/NavBar.js
+++ gigapet1/src/components/NavBar.js
@@ -26,7 +26,7 @@ export const NavBar = props => {
       <h1><a className="nodec" href="https://compassionate-lamarr-da90b8.netlify.com/">Lambdi Pet</a></h1>
       <div>
         <a className="nodec" href="https://compassionate-lamarr-da90b8.netlify.com/about.html">Our Team</a>
-        <a onClick={toggle}>{localStorage.getItem("token") ? "Menu" : "Welcome"}</a>
+        <a onClick={toggle}>{localStorage.getItem("token") ? "Menu" : " "}</a>
       </div>
     </nav>
     </>
