214336e3ab24bc485a0ff9e6976fbbd777b72985
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-01-08 18:23:56 +0500

    Helper for setting date value in iniput element

---
+++ gigapet1/src/helpers/index.js
@@ -0,0 +1,9 @@
+export function getCurrentDate() {
+    const date = new Date();
+    const day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+    const month =
+      date.getMonth() + 1 < 10 ? "0" + (date.getMonth()+1) : date.getMonth() + 1;
+    return (
+      date.getFullYear() + "-" + month + "-" + day
+    );
+  }
